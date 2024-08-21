35e7448108efe0c504fa782f4716c7b7e6e3f009
Ubaid5100 <167215122+Ubaid5100@users.noreply.github.com>
2024-08-21 11:54:50 +0500

    script.js

--- script.js
+++ script.js
@@ -2,38 +2,35 @@ const gameConfigs = {
     bike: {
         APP_TOKEN: 'd28721be-fd2d-4b45-869e-9f253b554e50',
         PROMO_ID: '43e35910-c168-4634-ad4f-52fd764a843f',
-        EVENTS_DELAY: 20000
-        attemptsNumber: 22,
+        EVENTS_DELAY: 2000
     },
     chainCub: {
         APP_TOKEN: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
         PROMO_ID: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
         EVENTS_DELAY: 22000
-        attemptsNumber: 30,
+        
     },
     cloneArmy: {
         APP_TOKEN: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
         PROMO_ID: 'fe693b26-b342-4159-8808-15e3ff7f8767',
         EVENTS_DELAY: 120000
-        attemptsNumber: 40,
+        
     },
     trainMiner: {
         APP_TOKEN: '82647f43-3f87-402d-88dd-09a90025313f',
         PROMO_ID: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
         EVENTS_DELAY: 120000
-        attemptsNumber: 20,
+        
     },
     mergeAway: {
         APP_TOKEN: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
         PROMO_ID: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
         EVENTS_DELAY: 22000
-        attemptsNumber: 22,
     },
     twerkRace: {
         APP_TOKEN: '61308365-9d16-4040-8bb0-2f4a4c69074c',
         PROMO_ID: '61308365-9d16-4040-8bb0-2f4a4c69074c',
         EVENTS_DELAY: 20000
-        attemptsNumber: 42,
     }
 };
 
