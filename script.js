f5be15787fb3c58db768d040885dfde32bd0ac94
Ubaid5100 <167215122+Ubaid5100@users.noreply.github.com>
2024-08-25 01:57:54 +0500

    script.js

--- script.js
+++ script.js
@@ -2,25 +2,22 @@ const gameConfigs = {
     bike: {
         APP_TOKEN: 'd28721be-fd2d-4b45-869e-9f253b554e50',
         PROMO_ID: '43e35910-c168-4634-ad4f-52fd764a843f',
-        EVENTS_DELAY: 2000
+        EVENTS_DELAY: 22000
     },
     chainCub: {
         APP_TOKEN: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
         PROMO_ID: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
         EVENTS_DELAY: 22000
-        
     },
     cloneArmy: {
         APP_TOKEN: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
         PROMO_ID: 'fe693b26-b342-4159-8808-15e3ff7f8767',
         EVENTS_DELAY: 120000
-        
     },
     trainMiner: {
         APP_TOKEN: '82647f43-3f87-402d-88dd-09a90025313f',
         PROMO_ID: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
         EVENTS_DELAY: 120000
-        
     },
     mergeAway: {
         APP_TOKEN: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
@@ -31,6 +28,21 @@ const gameConfigs = {
         APP_TOKEN: '61308365-9d16-4040-8bb0-2f4a4c69074c',
         PROMO_ID: '61308365-9d16-4040-8bb0-2f4a4c69074c',
         EVENTS_DELAY: 20000
+    },
+    polysphere: {
+        APP_TOKEN: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        PROMO_ID: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        EVENTS_DELAY: 3000
+    },
+    mow_and_Trim: {
+        APP_TOKEN: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        PROMO_ID: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+        EVENTS_DELAY: 31000
+    },
+    mud_Racing: {
+        APP_TOKEN: '8814a785-97fb-4177-9193-ca4180ff9da8',
+        PROMO_ID: '8814a785-97fb-4177-9193-ca4180ff9da8',
+        EVENTS_DELAY: 31000
     }
 };
 
@@ -82,7 +94,7 @@ document.getElementById('startBtn').addEventListener('click', async () => {
             return null;
         }
 
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < 16; i++) {
             await sleep(EVENTS_DELAY * delayRandom());
             const hasCode = await emulateProgress(clientToken, PROMO_ID);
             updateProgress(9 / keyCount);
